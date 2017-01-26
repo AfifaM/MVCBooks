68622fd1bc1246a2c89f064f31434099187efc9b
AfifaM <afifa.moosa@hotmail.com>
2017-01-26 13:07:53 +0000

    MVC Books

---
+++ MvcBooks/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcBooks.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "Release date is required")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 100), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Picture")]
+        public string Picture { get; set; }
+
+        
+    }
+}
